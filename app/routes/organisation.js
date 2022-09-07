16c23886325b74a5a3dfa1ebb5e07e1f8f0ce939
Adam Silver <adambsilver@gmail.com>
2022-09-07 08:16:06 +0100

    Logo flow

--- app/routes/organisation.js
+++ app/routes/organisation.js
@@ -7,4 +7,14 @@ module.exports = router => {
     res.render('organisation/index')
   })
 
+  router.post('/organisation/edit-logo', authentication.checkIsAuthenticated, (req, res) => {
+    req.session.data.logo = '/public/images/logos/courtland.png'
+    res.redirect('/organisation/edit-logo/check')
+  })
+
+  router.post('/organisation/edit-logo/check', authentication.checkIsAuthenticated, (req, res) => {
+    req.session.user.organisation.logo = req.session.data.logo
+    res.redirect('/organisation')
+  })
+
 }
