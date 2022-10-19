b489041b03b46d26b4d78034d739a7767235aa26
Adam Silver <adambsilver@gmail.com>
2022-10-19 14:16:59 +0100

    Invitees page

--- app/routes/jobs.js
+++ app/routes/jobs.js
@@ -15,6 +15,14 @@ module.exports = router => {
     })
   })
 
+  router.get('/jobs/:id/invitees', authentication.checkIsAuthenticated, (req, res) => {
+    let job = req.session.user.jobs.find(job => job.id == req.params.id)
+
+    res.render('jobs/invitees', {
+      job
+    })
+  })
+
   router.get('/jobs/:id/preview', authentication.checkIsAuthenticated, (req, res) => {
     let job = req.session.user.jobs.find(job => job.id == req.params.id)
 
