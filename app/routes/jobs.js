68b70a559e44548d98e663a9ee6a03d946cf5907
alexleedfe <115729523+alexleedfe@users.noreply.github.com>
2024-06-26 15:09:48 +0100

    adding job listings applications tab functionality with example of application review with redesign

--- app/routes/jobs.js
+++ app/routes/jobs.js
@@ -22,6 +22,22 @@ module.exports = router => {
     })
   })
 
+  router.get('/jobs/:id/applications', authentication.checkIsAuthenticated, (req, res) => {
+    let job = req.session.user.jobs.find(job => job.id == req.params.id)
+
+    res.render('jobs/applications', {
+      job
+    })
+  })
+
+  router.get('/jobs/:id/applications_example', authentication.checkIsAuthenticated, (req, res) => {
+    let job = req.session.user.jobs.find(job => job.id == req.params.id)
+
+    res.render('jobs/applications_example', {
+      job
+    })
+  })
+
   router.get('/jobs/:id/invitees', authentication.checkIsAuthenticated, (req, res) => {
     let job = req.session.user.jobs.find(job => job.id == req.params.id)
 
